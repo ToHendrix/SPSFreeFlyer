a86aae4fc2b142b99dff9f656925b97ff6c670c6
Jesper Frijns <jesperfrijns@hotmail.com>
2019-06-05 11:23:59 +0200

    Finished Database

--- Python/Database.py
+++ Python/Database.py
@@ -38,30 +38,38 @@ def dataset(file):
     if file == 'Aero_data.txt':
         del temp[0:89]
         del temp[-1]
-        temp.insert(0,['Density [cm^-3]', 'FFlux [cm^-2 s^-1]', 
+        temp.insert(0,['N_0 [cm^-3]', 'FFlux [cm^-2 s^-1]', 
                     'BFlux [cm^-2 s^-1]', 'FFluence [cm^-2]', 
                     'BFluence [cm^-2]', 'TMD [g cm^-3]', 'Angle [deg]'])
     if file == 'Coordinate_data.txt':
         del temp[0:73]
         del temp[-1]
+#       Alpha is the pitch angle (not important) 
         temp.insert(0,['MJD [days]', 'Altitude [km]', 'Latitude [deg]', 
                        'Longitude [deg]', 'Local Time [hrs]', 'Alpha [deg]'])
     if file == 'Vector_data.txt':
         del temp[0:73]
         del temp[-1]
-        temp.insert(0,['Attitude_0 [?]', 'Attitude_1 [?]', 'Attitude_2 [?]', 
-                       'Attitude_3 [?]', 'Attitude_4 [?]', 'Attitude_5 [?]', 
-                       'Attitude_6 [?]', 'Attitude_7 [?]', 'Attitude_8 [?]', 
-                       'V_1 [km s^-1]', 'V_2 [km s^-1]', 'V_3 [km s^-1]', 
-                       'V_Sun_1 [km s^-1]', 'V_Sun_2 [km s^-1]', 
-                       'V_Sun_3 [km s^-1]'])
+#       Attitude contains the 9 values for rotation matrix w.r.t. reference 
+#       frame (1_rho, 1_theta, 1_phi) (https://orfeo.kbr.be/bitstream/handle/internal/6594/Heynderickx%281996j%29.pdf?sequence=1&isAllowed=y)
+#       Axes order: , X (In orbital plane, away from earth), 
+#       Y (South, perpendicular to orbital plane), Z (parallel to V)
+        temp.insert(0,['Attitude_0 [-]', 'Attitude_1 [-]', 'Attitude_2 [-]', 
+                       'Attitude_3 [-]', 'Attitude_4 [-]', 'Attitude_5 [-]', 
+                       'Attitude_6 [-]', 'Attitude_7 [-]', 'Attitude_8 [-]', 
+                       'V_x [km s^-1]', 'V_y [km s^-1]', 'V_z [km s^-1]', 
+                       'Sun_norm_x [-]', 'Sun_norm_y [-]', 
+                       'Sun_norm_z [-]'])
     if file == 'Magnetic_field_data.txt':
         del temp[0:88]
         del temp[-1]
+#       B_SPH_RHO is the projection of the magnetic field on the radial axis, i.e. the vertical component of the magnetic field;
+#   	B_SPH_THETA is the southward component of the magnetic field after projection in the horizontal plane;
+#       B_SPH_PHI is the eastward component of the magnetic field after projection in the horizontal plane
         temp.insert(0,['B_LOC [Gauss]','L_LOC [?]','B_ALP [Gauss]','L_ALP [?]',
                        'I_LOC [?]','I_ALP [Re]','B_SPH_RHO [Gauss]',
-                       'B_SPH_THETA [Gauss]','B_SPH_PHI [Gauss]','N_SPH_1 [-?]',
-                       'N_SPH_2 [-?]','N_SPH_3 [-?]'])
+                       'B_SPH_THETA [Gauss]','B_SPH_PHI [Gauss]','N_SPH_RHO [-?]',
+                       'N_SPH_THETA [-?]','N_SPH_PHI [-?]'])
     
 
     return temp
