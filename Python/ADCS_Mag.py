0bd796720bf530360d3ee4cf3358b2a3ff79dd18
[JJMFrijns] <[jesperfrijns@hotmail.com]>
2019-06-07 15:45:03 +0200

    Finished comments on ADCS_Mag.py

--- Python/ADCS_Mag.py
+++ Python/ADCS_Mag.py
@@ -18,7 +18,7 @@ import numpy as np
 # =============================================================================
 # Defining arrays and variables
 # =============================================================================
-M_res_dip = np.array([[0.2],                    #Residual Dipole Moment [A m^2]
+M_res_dip = np.array([[0.2],                                                   #Residual Dipole Moment [A m^2]
                     [0.2],
                     [0.2]])
 M_ext_magn = np.zeros((43201,3))
@@ -26,14 +26,12 @@ M_ext_magn = np.zeros((43201,3))
 # =============================================================================
 # Initialising the arrays used
 # =============================================================================
+magn_sph_data = np.delete(magn_sph_data, np.s_[0:6], axis=1)                   #Cropping the magnetic field array to get spherical coordinates
+magn_sph_data = np.delete(magn_sph_data, np.s_[-3:], axis=1)                   #Cropping the magnetic field array to get spherical coordinates 
+magn_sph_data[:,1] = -magn_sph_data[:,1]                                       #Changing the direction of the soutward magnetic field component to be pointing north 
+magn_E_data = magn_sph_data                                                    #Writing the magnetic field array to the correct named variable 
 
-# Cropping the magnetic field array
-magn_sph_data = np.delete(magn_sph_data, np.s_[0:6], axis=1)
-magn_sph_data = np.delete(magn_sph_data, np.s_[-3:], axis=1)
-magn_sph_data[:,1] = -magn_sph_data[:,1]
-magn_E_data = magn_sph_data
-
-magn_P_data = E_to_P(magn_E_data)
+magn_P_data = E_to_P(magn_E_data)                                              #Tranforming the magnetic field from the E- to P-frame         
 
 for i in range(len(magn_P_data)):
-    M_ext_magn[i] = np.cross(np.transpose(M_res_dip), [magn_P_data[i]])
+    M_ext_magn[i] = np.cross(np.transpose(M_res_dip), [magn_P_data[i]])        #Calculating magnetic field disturbance torque at every instance     
