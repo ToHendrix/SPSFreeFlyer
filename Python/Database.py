897e910ac3e3e137adbee5d3093a7df3da6d7a89
Tom Hendrix <thendrix@tudelft.net>
2019-06-07 14:30:23 +0200

    Finished database

--- Python/Database.py
+++ Python/Database.py
@@ -21,45 +21,45 @@ import numpy as np
 f       = 1./298.2                                                             #Flattening of the earth [-]
 a_Earth = 6378160.                                                             #Semi-major axis Earth [m]
 b_Earth = a_Earth*(1. - f)                                                     #Semi-minor axis Earth [m] 
-t_yr = 31557600.                                                               #Duration year [s] 
+
 # =============================================================================
 # Creating the databases
 # =============================================================================
-def dataset(file):
-    temp = []
-    
-    f = open(file,"r") # Open file for reading
-    lines = f.readlines()
-    f.close()
-    for s in lines:
-        temp.append(s.replace(',', '').split())
+def dataset(file):                                                             #Definition that reads all data files to arrays
+    temp = []                                                                  #Temporary to append an array line to      
+    f = open(file,"r")                                                         #Open file for reading
+    lines = f.readlines()                                                      #Read lines 
+    f.close()                                                                  #Close file         
+    for s in lines:                                 
+        temp.append(s.replace(',', '').split())                                #Append a line to temp of which commas have been removed and has been split with the help of spacebars     
     
     if file == 'Aero_data.txt':
-        del temp[0:89]
-        del temp[-1]
-        temp = np.asarray(temp).astype(np.float)
-#        temp = np.vstack((np.array(['N_0 [cm^-3]', 'FFlux [cm^-2 s^-1]', 
+        del temp[0:89]                                                         #Deleting all non-numbers in front 
+        del temp[-1]                                                           #Deleting all non-numbers in back
+        temp = np.asarray(temp).astype(np.float)                               #Make an array of the list and floats of all values inside
+#        temp = np.vstack((np.array(['N_0 [cm^-3]', 'FFlux [cm^-2 s^-1]',      #Legend 
 #                                    'BFlux [cm^-2 s^-1]', 'FFluence [cm^-2]', 
 #                                    'BFluence [cm^-2]', 'TMD [g cm^-3]', 
 #                                    'Angle [deg]']), temp))
     if file == 'Coordinate_data.txt':
-        del temp[0:73]
-        del temp[-1]
-        temp = np.asarray(temp).astype(np.float)
-        temp[:,2] = np.deg2rad(temp[:,2])
-        temp[:,3] = np.deg2rad(temp[:,3])
-#        temp = np.vstack((np.array(['MJD [days]', 'Altitude [km]', 'Latitude [rad]', 
-#                                    'Longitude [rad]', 'Local Time [hrs]', 
-#                                    'Alpha [deg]']), temp))
+        del temp[0:73]                                                         #Deleting all non-numbers in front              
+        del temp[-1]                                                           #Deleting all non-numbers in back             
+        temp = np.asarray(temp).astype(np.float)                               #Make an array of the list and floats of all values inside 
+        temp[:,2] = np.deg2rad(temp[:,2])                                      #Change 3rd column from degrees to radians     
+        temp[:,3] = np.deg2rad(temp[:,3])                                      #Change 4th column from degrees to radians      
+#        temp = np.vstack((np.array(['MJD [days]', 'Altitude [km]', 'Latitude  #Legend 
+#                                    [rad]', 'Longitude [rad]', 'Local Time 
+#                                    [hrs]', 'Alpha [deg]']), temp))
+
     if file == 'Vector_data.txt':
-        del temp[0:73]
-        del temp[-1]
+        del temp[0:73]                                                         #Deleting all non-numbers in front    
+        del temp[-1]                                                           #Deleting all non-numbers in back       
 #       -Attitude contains the 9 values for rotation matrix w.r.t. reference 
 #        frame (1_rho, 1_theta, 1_phi) (https://orfeo.kbr.be/bitstream/handle/internal/6594/Heynderickx%281996j%29.pdf?sequence=1&isAllowed=y)
 #       -Axes order: , X (In orbital plane, away from earth), 
 #        Y (South, perpendicular to orbital plane), Z (parallel to V)
-        temp = np.asarray(temp).astype(np.float)
-#        temp = np.vstack((np.array(['Attitude_0 [-]', 'Attitude_1 [-]', 
+        temp = np.asarray(temp).astype(np.float)                               #Make an array of the list and floats of all values inside      
+#        temp = np.vstack((np.array(['Attitude_0 [-]', 'Attitude_1 [-]',       #Legend 
 #                                    'Attitude_2 [-]', 'Attitude_3 [-]', 
 #                                    'Attitude_4 [-]', 'Attitude_5 [-]', 
 #                                    'Attitude_6 [-]', 'Attitude_7 [-]', 
@@ -68,34 +68,33 @@ def dataset(file):
 #                                    'Sun_norm_x [-]', 'Sun_norm_y [-]', 
 #                                    'Sun_norm_z [-]']), temp))
     if file == 'Magnetic_field_data.txt':
-        del temp[0:88]
-        del temp[-1]
+        del temp[0:88]                                                         #Deleting all non-numbers in front         
+        del temp[-1]                                                           #Deleting all non-numbers in back              
 #       -B_SPH_RHO is the projection of the magnetic field on the radial axis, 
 #        i.e. the vertical component of the magnetic field;
 #   	-B_SPH_THETA is the southward component of the magnetic field after 
 #        projection in the horizontal plane;
 #       -B_SPH_PHI is the eastward component of the magnetic field after 
 #        projection in the horizontal plane
-        temp = np.asarray(temp).astype(np.float)
-#        temp = np.vstack((np.array(['B_LOC [Gauss]','L_LOC [?]','B_ALP [Gauss]'
+        temp = np.asarray(temp).astype(np.float)                               #Make an array of the list and floats of all values inside     
+#        temp = np.vstack((np.array(['B_LOC [Gauss]','L_LOC [?]','B_ALP [Gauss]' #Legend
 #                                    ,'L_ALP [?]', 'I_LOC [?]','I_ALP [Re]',
 #                                    'B_SPH_RHO [Gauss]', 'B_SPH_THETA [Gauss]',
 #                                    'B_SPH_PHI [Gauss]','N_SPH_RHO [-?]',
 #                                    'N_SPH_THETA [-?]','N_SPH_PHI [-?]']), 
 #                                    temp))   
-    return temp
+    return temp                                                                #Returning the final dataset array 
 
-aero_data = np.array(dataset('Aero_data.txt'))
-coor_elli_data = np.array(dataset('Coordinate_data.txt'))
-vect_C_data = np.array(dataset('Vector_data.txt'))
-magn_data = np.array(dataset('Magnetic_field_data.txt'))
+aero_data = np.array(dataset('Aero_data.txt'))                                 #Calling up the creation of aero dataset 
+coor_elli_data = np.array(dataset('Coordinate_data.txt'))                      #Calling up the creation of coordinate dataset  
+vect_C_data = np.array(dataset('Vector_data.txt'))                             #Calling up the creation of vector dataset  
+magn_data = np.array(dataset('Magnetic_field_data.txt'))                       #Calling up the creation of magnetic field dataset  
 
 # =============================================================================
 # Converting the coordinate database from ellipsoidal coordinates to Cartesian
 # coordinates
 # =============================================================================
-
-def elli_to_C():
+def elli_to_C():                                                               #     
     temp = np.zeros((43201,5))
     for i in range(len(coor_elli_data)):
         N   = a_Earth*(1. - f*(2. - f) * \
@@ -118,9 +117,8 @@ coor_C_data = elli_to_C()                                                      #
 # =============================================================================
 # Converting the coordinate database from the C frame to the E frame
 # =============================================================================
-
-def C_to_E(C_frame, angles):
-    temp = np.zeros((43201,3))
+def C_to_E(C_frame, angles):                                                   #Definition to transform from the C- to the E-frame 
+    temp = np.zeros((43201,3))                                                 #Creating initial array to append to 
     
     for i in range(len(C_frame[:,0])):
         T_EC = np.array([[-np.sin(angles[i,2])*np.cos(angles[i,3]), \
@@ -131,28 +129,37 @@ def C_to_E(C_frame, angles):
                           -np.cos(angles[i,2])*np.sin(angles[i,3]), \
                           -np.sin(angles[i,2])]])
         
-        temp[i] = np.dot(T_EC, C_frame[i,:3])
+        temp[i] = np.dot(T_EC, C_frame[i,:3])                                  #Transforming every data point in the line used 
         
     return temp
 
-coor_E_data = C_to_E(coor_C_data, coor_elli_data)
+coor_E_data = C_to_E(coor_C_data, coor_elli_data)                              #Calling up the transformation from coordinate C- to E-frame 
 
 # =============================================================================
 # Converting the coordinate database from the E frame to the P frame
 # =============================================================================
+Sun_E_data = C_to_E(vect_C_data[:,-3:], coor_elli_data)                        # Converting the Sun vector from the E frame to the P frame 
 
-# Converting the Sun vector from the E frame to the P frame
-Sun_E_data = C_to_E(vect_C_data[:,-3:], coor_elli_data)
+def E_to_P(E_frame):                                                           #Definition to transform from the C- to the E-frame with the z-axis pointing at the Sun 
+    temp = np.zeros((43201,3))
+    
+    for i in range(len(E_frame[:,0])):
+        alpha = np.arctan2(Sun_E_data[i,1], Sun_E_data[i,2]) -\
+        np.arctan2(coor_E_data[i,1], coor_E_data[i,2])  
+        beta = np.arctan2(Sun_E_data[i,0], Sun_E_data[i,2]) - \
+        np.arctan2(coor_E_data[i,0], coor_E_data[i,2])
 
-#Calculating the transformation angles
+        T_PE = np.array([[np.cos(beta), np.sin(beta)*np.sin(alpha), \
+                          np.sin(beta)*np.cos(beta)],
+                 [0., np.cos(alpha), -np.sin(alpha)],
+                 [-np.sin(beta), np.cos(beta)*np.sin(alpha), \
+                  np.cos(beta)*np.cos(alpha)]])
+        
+        temp[i] = np.dot(T_PE, E_frame[i,:3])
+        
+    return temp
 
-a = 
-
-
-#alpha, beta gamma 
-
-
-
+coor_P_data = E_to_P(coor_E_data)
 
 
 
