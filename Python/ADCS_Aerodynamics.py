506829e8cdc1bebe68efb4ed723e8f3a0b0dea7e
Sasha <gmail@sasha.com>
2019-06-10 19:43:15 +0200

    Changing the file to

--- Python/ADCS_Aerodynamics.py
+++ Python/ADCS_Aerodynamics.py
@@ -1,6 +1,16 @@
 # Import necessary things
 import scipy
 import numpy as np
+from Database import *
+
+# ------------------------------------ Import Stuff from the Database --------------------------------------------------
+# velocity data
+Vx = vect_C_data[:,9] # km/s
+Vy = vect_C_data[:,10] # km/s
+Vz = vect_C_data[:,11] # km/s
+V = ((1000*Vx)**2 + (1000*Vy)**2 + (1000*Vz)**2)**0.5 # m/s
+
+
 
 # ------------------------------------ Define constants -----------------------------------------------------------------
 h = 0.455  # [m], height of the SPS
@@ -8,20 +18,19 @@ d = 1.  # [m], diameter of SPS
 C_D = 2.
 
 # ------------------------------------ Define Vectors -------------------------------------------------------------------
-Vu = np.array((1, 0, 0))  # [m/s], unit velocity vector. This is not yet determined, and needs a direction function
+Vu = np.array([Vx, Vy, Vz])/V  # [m/s], unit velocity vector
 r_plate = np.array((0, 0, .5 * h))  # [m]
-r_wall = np.array((0, 0, 9.11))  # [m] ("not", "sure", "yet")
+r_wall = np.array((0, 0, 9.11))  # [m] unsure about this, evaluate worst-case?
 
 # ------------------------------------ assumed values -------------------------------------------------------------------
-V = 7000.  # [m/s]
 beta = 0.5  # [rad], this is wrong
 rho = 0.0000000000001  # [kg/m3]
 
 
 # ------------------------------------ Define functions -----------------------------------------------------------------
 def forces(C_D, rho, d):
-    F_plate = .5 * C_D * rho * (np.pi * d / 2.) * V ** (2.) * (Vu)
-    F_wall = -0.5 * C_D * (2. * np.pi * d / 2. * h) * V ** (2.) * (Vu * np.sin(beta))
+    F_plate = .5 * C_D * rho * (np.pi * d / 2.) * V**2 * (Vu)
+    F_wall = -0.5 * C_D * (2. * np.pi * d / 2. * h) * V**2 * (Vu * np.sin(beta))
     return F_plate, F_wall
 
 
@@ -34,5 +43,9 @@ def moments(r_plate, r_wall, F_plate, F_wall):
 # ------------------------------------ Test -----------------------------------------------------------------------------
 Forces = forces(C_D, rho, d)
 Moments = moments(r_plate, r_wall, Forces[0], Forces[1])
-print(Forces)
-print(Moments)
+#print(((vect_C_data[9][10]*1000)**2 + (vect_C_data[10][10]*1000)**2 + (vect_C_data[11][10]*1000)**2))
+#
+# print(len(aero_data[:,1]))
+# print((aero_data[:,1]))
+# print((aero_data[:,1])**2**0.5)
+
